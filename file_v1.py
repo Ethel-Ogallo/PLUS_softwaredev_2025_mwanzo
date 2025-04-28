f6095c4e3d93c78a53f76c265e9f745f601c2ff6
Ethel Ogallo <ogalloethel@gmail.com>
2025-04-28 11:50:56 +0200

    update

--- file_v1.py
+++ file_v1.py
@@ -8,3 +8,4 @@ days_of_the_week = 7
 def say_hello(recipient):
     print("Hello, world! Hello {}".format(recipient))
     return recipient
+
---
+++ file_v2.py
@@ -0,0 +1,6 @@
+
+def get_crs(location):
+    """ This function gives back the CRS of 
+    the location which should be geodataframe
+    """
+    return location.crs
